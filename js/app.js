26553da4aa022d504ab27617a5cfe0f3c3ea5ef7
gabrielamf <glennygabb@gmail.com>
2017-12-27 03:35:30 -0500

    añade imagenes a html con jquery

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
 $(window).ready(function() {
-	$('.splash').delay(3000).fadeOut(1000);
-
-
-
-
-
-
-
-
-
-
-
+  $('.splash').delay(3000).fadeOut(1000);
+	
+  var container = $('#container-restaurants');
+  for (i = 0; i < data.length; i++) {
+    container.append('<div class="col-xs-4"><div class="img-box"><p class="name-restaurant">' + data[i].name + '</p><div class="overlay"></div><img class="img-responsive img-restaurant" src=' + data[i].image + '></div></div>');
+  }
 });
