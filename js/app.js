946e9212893886eff922bc553126769def948841
gabrielamf <glennygabb@gmail.com>
2017-12-30 07:01:10 -0500

    añade modal

--- js/app.js
+++ js/app.js
@@ -7,13 +7,13 @@ $(window).ready(function() {
 	
   var container = $('#container-restaurants');
   for (i = 0; i < data.length; i++) {
-    container.append(
-      '<div class="col-xs-4 photo collection text-center flip">' +
-				'<img class="img-responsive flip-1" src = ' + data[i].image + '>' +
-				'<p class="type-restaurant">' + data[i].type + '</p>' +
-				'<img class="img-responsive flip-2" src = ' + data[i].back + '>' +
-			'</div>'
-    );
+    var grupo = 
+		'<div class="col-xs-4 photo collection text-center flip">' +
+			'<img class="img-responsive flip-1 content" src = ' + data[i].image + ' data-target="#imgModal" data-toggle="modal" data-name= "' + data[i].name + '" data-address= "' + data[i].address + '" data-phone= "' + data[i].phone + '" data-attention= "' + data[i].attention + '" data-type= "' + data[i].type + '" data-money= "' + data[i].money + '">' + 
+			'<p class="type-restaurant">' + data[i].type + '</p>' +
+			'<img class="img-responsive flip-2 content" src = ' + data[i].back + ' data-target="#imgModal" data-toggle="modal">' +
+		'</div>';
+    container.append(grupo);
   };
 
   $('#search').keyup(function() {
@@ -25,6 +25,21 @@ $(window).ready(function() {
         $(this).show();
       }
     });
+  });
+
+  $('.content').click(function() {
+    var name = $(this).data('name'),
+    	address = $(this).data('address'),
+      phone = $(this).data('phone'),
+      attention = $(this).data('attention'),
+      money = $(this).data('money'),
+      type = $(this).data('type');
+    $('.name').text(name);
+    $('.address').text(address);
+    $('.phone').text(phone);
+    $('.attention').text(attention);
+    $('.money').text(money);
+    $('.type').text(type);
   });
 	
   $('.collection').mouseover(function() {
